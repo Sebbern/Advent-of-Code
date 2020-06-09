4865483b0a95c5103722f96271d2fb8241d56143
Sebbern <Sebastianeckbo@gmail.com>
2020-06-09 12:18:23 +0200

    2018 day 1 part 2

--- 2018/day1/day1.py
+++ 2018/day1/day1.py
@@ -1,7 +1,21 @@
+import itertools
+
 freqlist = open("input.txt", "r").read().split()
+# Part one
 freq = 0
 
 for i in freqlist:
     freq += int(i)
 
 print(freq)
+
+# Part two
+freqtwo = 0
+sumlist = {0}
+
+for i in itertools.cycle(freqlist):
+    freqtwo += int(i)
+    if freqtwo in sumlist:
+        print(freqtwo)
+        break
+    sumlist.add(freqtwo)
