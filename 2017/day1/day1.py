daac1482ff2f1d90466bfabab627f80f4d4a0c02
Sebbern <Sebastianeckbo@gmail.com>
2020-06-09 13:40:32 +0200

    2017 day1 part 2

--- 2017/day1/day1.py
+++ 2017/day1/day1.py
@@ -1,9 +1,10 @@
 captcha = open("input.txt", "r").read()
+captchalist = [x for x in str(captcha)]
+
+# Part one
 sumcaptcha = 0
 u = 0
 y = 0
-
-captchalist = [x for x in str(captcha)]
 
 for i in captchalist:
     y = u
@@ -15,3 +16,21 @@ if captchalist[0] == captchalist[-1]:
     sumcaptcha += int(captchalist[0])
 
 print(sumcaptcha)
+
+# Part two
+sumcaptchatwo = 0
+halfoflist = int(len(captchalist)/2)
+uu = 0
+
+for i, ii in enumerate(captchalist):
+    uu = int(ii)
+    i += halfoflist
+    try:
+        ii = captchalist[i]
+    except:
+        IndexError
+        break
+    if uu == int(ii):
+        sumcaptchatwo += int(ii) * 2
+    
+print(sumcaptchatwo)
