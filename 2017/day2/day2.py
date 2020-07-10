5449d66e9a9f41c8d0b63b739413acd1990648c2
Sebbern <Sebastianeckbo@gmail.com>
2020-07-10 06:13:31 +0200

    fix variable name

--- 2017/day2/day2.py
+++ 2017/day2/day2.py
@@ -1,12 +1,12 @@
 checksumlist = open("input.txt", "r").read().splitlines()
 
 def checksum(s):
-    sum = 0
+    checksum = 0
     difference = 0
     for u in checksumlist:
         s = u.split('\t')
         difference = int(max(s, key=int)) - int(min(s, key=int))
-        sum += difference
-    print(sum)
+        checksum += difference
+    print(checksum)
 
 checksum([])
