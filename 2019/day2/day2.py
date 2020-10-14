efbbb5bf388172f969dac9577e8585b702984f17
Sebbern <Sebastianeckbo@gmail.com>
2020-10-14 21:04:53 +0200

    add day 2 2019

--- 2019/day2/day2.py
+++ 2019/day2/day2.py
@@ -1,4 +1,17 @@
-intcode = open("input.txt", "r").read().split()
+intcode = open("input.txt", "r").read().split(",")
+position = 0
 
+intcode[1] = 12
+intcode[2] = 2
 
-print(intcode)
+for i in intcode:
+    i = int(i)
+    if position % 4 == 0:
+        if i == 99:
+            print(intcode[0])
+            break
+        if i == 1:
+            intcode[int(intcode[position+3])] = int(intcode[int(intcode[position+1])]) + int(intcode[int(intcode[position+2])])
+        if i == 2:
+            intcode[int(intcode[position+3])] = int(intcode[int(intcode[position+1])]) * int(intcode[int(intcode[position+2])])
+    position += 1
