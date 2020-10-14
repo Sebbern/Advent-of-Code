d7b74e3da9825cf98848fd470f657ecb44690d21
Sebbern <Sebastianeckbo@gmail.com>
2020-10-14 22:05:27 +0200

    add day 2 part 2 2019

--- 2019/day2/day2.py
+++ 2019/day2/day2.py
@@ -1,17 +1,16 @@
-intcode = open("input.txt", "r").read().split(",")
+intcode = [int(i) for i in open("input.txt", "r").read().split(",")]
 position = 0
 
 intcode[1] = 12
 intcode[2] = 2
 
-for i in intcode:
-    i = int(i)
+while True:
     if position % 4 == 0:
-        if i == 99:
+        if intcode[position] == 99:
             print(intcode[0])
             break
-        if i == 1:
+        if intcode[position] == 1:
             intcode[int(intcode[position+3])] = int(intcode[int(intcode[position+1])]) + int(intcode[int(intcode[position+2])])
-        if i == 2:
+        if intcode[position] == 2:
             intcode[int(intcode[position+3])] = int(intcode[int(intcode[position+1])]) * int(intcode[int(intcode[position+2])])
     position += 1
---
+++ 2019/day2/day2_2.py
@@ -0,0 +1,19 @@
+intcode = [int(i) for i in open("input.txt", "r").read().split(",")]
+
+for noun in range(100):
+    for verb in range(100):
+        position = 0
+        program = intcode[:]
+        program[1] = noun
+        program[2] = verb
+        while True:
+            if position % 4 == 0:
+                if program[position] == 99:
+                    break
+                if program[position] == 1:
+                    program[int(program[position+3])] = int(program[int(program[position+1])]) + int(program[int(program[position+2])])
+                if program[position] == 2:
+                    program[int(program[position+3])] = int(program[int(program[position+1])]) * int(program[int(program[position+2])])
+            position += 1
+        if program[0] == 19690720:
+            print(100 * noun + verb)
