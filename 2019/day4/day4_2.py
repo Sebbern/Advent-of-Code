6516fc8e451d04ca376d34e193b44a61bf757be9
Sebbern <Sebastianeckbo@gmail.com>
2020-10-15 14:32:17 +0200

    remove clutter

--- 2019/day4/day4_2.py
+++ 2019/day4/day4_2.py
@@ -3,11 +3,11 @@ maxpuzzle = 767346
 passwords = []
 
 for i in range(minpuzzle, maxpuzzle + 1):
-    for a,b,c,d,e,f in zip(str(i)[0:],str(i)[1:],str(i)[2:],str(i)[3:],str(i)[4:],str(i)[5:]):
-        if a <= b <= c <= d <= e <= f:
-            for u in str(i):
-                if str(i).count(u) == 2:
-                    passwords.append(i)
-                    break
+    i = [int(x) for x in str(i)]
+    if i == sorted(i):
+        for u in i:
+            if i.count(u) == 2:
+                passwords.append(i)
+                break
 
 print(len(passwords))
