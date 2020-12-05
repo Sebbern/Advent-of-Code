ce10a8fe0580977dcc8379a8c0e0a427bfd14179
Sebbern <Sebastianeckbo@gmail.com>
2020-12-05 13:16:49 +0100

    add day5

---
+++ 2020/day5/day5.py
@@ -0,0 +1,34 @@
+boardingpasses = open("input.txt", "r").read().splitlines()
+
+seatidlist = []
+
+for i in boardingpasses:
+    rlist = [*range(0, 128)]
+    clist = [*range(0, 8)]
+    row = 0
+    column = 0
+    seatid = 8
+
+    for u in i:
+        upper = len(rlist)
+        lower = 0
+        seatupper = len(clist)
+        seatlower = 0
+        if u == "F":
+            upper = upper / 2
+            rlist = rlist[0:int(upper)]
+        elif u == "B":
+            lower += (len(rlist) / 2)
+            rlist = rlist[int(lower):]
+        elif u == "L":
+            seatupper = seatupper / 2
+            clist = clist[0:int(seatupper)]
+        elif u == "R":
+            seatlower += (len(clist) / 2)
+            clist = clist[int(seatlower):]
+            
+    row = rlist[0]
+    column = clist[0]
+    seatidlist.append(row * seatid + column)
+
+print(max(seatidlist))
