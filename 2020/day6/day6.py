2b2626746994b3f1e9de2d1b75a30eff77cb3259
Sebbern <Sebastianeckbo@gmail.com>
2020-12-06 13:56:54 +0100

    add day 6 part 2

--- 2020/day6/day6.py
+++ 2020/day6/day6.py
@@ -7,8 +7,10 @@ for i in answers:
     if not i:
         uniqueyes = set(groupyes)
         yes += len(uniqueyes)
+
         groupyes = []
         uniqueyes = []
+        
     for u in i:
         groupyes.append(u)
 
