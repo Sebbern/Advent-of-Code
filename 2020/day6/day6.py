c7966b0d85a5c6f89c04f146d7e26d930ccf61e7
Sebbern <Sebastianeckbo@gmail.com>
2020-12-06 13:32:15 +0100

    add day6

---
+++ 2020/day6/day6.py
@@ -0,0 +1,15 @@
+answers = open("input.txt", "r").read().split("\n")
+
+groupyes = []
+yes = 0
+
+for i in answers:
+    if not i:
+        uniqueyes = set(groupyes)
+        yes += len(uniqueyes)
+        groupyes = []
+        uniqueyes = []
+    for u in i:
+        groupyes.append(u)
+
+print(yes)
