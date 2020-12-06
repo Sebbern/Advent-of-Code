9c030cd02456a613eb4ac21f34c8d635e968f024
Sebbern <Sebastianeckbo@gmail.com>
2020-12-06 13:58:40 +0100

    add day 6 part 2

---
+++ 2020/day6/day6_2.py
@@ -0,0 +1,23 @@
+answers = open("input.txt", "r").read().split("\n")
+
+groupyes = []
+yes = 0
+person = 0
+
+for i in answers:
+    if not i:
+        allyes = list(set([x for x in groupyes if groupyes.count(x) >= person]))
+        yes += len(allyes)
+
+        groupyes = []
+        uniqueyes = []
+        person = 0
+
+        continue
+
+    for u in i:
+        groupyes.append(u)
+        
+    person += 1
+
+print(yes)
