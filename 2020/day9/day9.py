3c314d4e0a21029199b4cd7709f864c425993eaf
Sebbern <Sebastianeckbo@gmail.com>
2020-12-09 06:44:11 +0100

    add day9

---
+++ 2020/day9/day9.py
@@ -0,0 +1,22 @@
+numbers = open("input.txt", "r").read().splitlines()
+
+preamble = []
+result = 0
+
+for i in numbers:
+    if len(preamble) > 25:
+        preamble = preamble[-25:]
+
+    if len(preamble) == 25:
+        for x in preamble:
+            for y in preamble:
+                if int(x) + int(y) == int(i):
+                    result += 1
+        
+        if result == 0:
+            print(i)
+        
+        result = 0
+
+    preamble.append(i)
+
