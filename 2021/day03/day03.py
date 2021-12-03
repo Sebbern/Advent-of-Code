5accbc8f57cf50fc5e07584b475312e1d458ae37
Sebbern <Sebastianeckbo@gmail.com>
2021-12-03 16:03:39 +0100

    add 2021 day 3

---
+++ 2021/day03/day03.py
@@ -0,0 +1,27 @@
+Binary = open("input.txt", "r").read().split()
+binarycount = 0
+zerocount = 0
+onecount = 0
+gammarate = [""]
+epsilonrate = [""]
+
+while binarycount < 12:
+    for i in Binary:
+        if i[binarycount] == "0":
+            zerocount += 1
+        elif i[binarycount] == "1":
+            onecount += 1
+        
+    if onecount > zerocount:
+        gammarate[0] = gammarate[0] + "1"
+        epsilonrate[0] = epsilonrate[0] + "0"
+    elif zerocount > onecount:
+        gammarate[0] = gammarate[0] + "0"
+        epsilonrate[0] = epsilonrate[0] + "1"
+
+    zerocount = 0
+    onecount = 0
+    binarycount += 1
+
+powerconsumption = int(gammarate[0], 2) * int(epsilonrate[0], 2)
+print(powerconsumption)
---
+++ 2021/day03/day03_2.py
@@ -0,0 +1,50 @@
+Binary = open("input.txt", "r").read().split()
+binarycount = 0
+zerocount = 0
+onecount = 0
+realbinary = ""
+co2binarylist = Binary
+co2binary = ""
+
+while binarycount < 12:
+    for i in Binary:
+        if i[binarycount] == "0":
+            zerocount += 1
+        elif i[binarycount] == "1":
+            onecount += 1
+
+    if zerocount > onecount:
+        realbinary += "0"
+    elif onecount > zerocount or onecount == zerocount:
+        realbinary += "1"
+        
+    if len(Binary) > 1:
+        Binary = [x for x in Binary if x.startswith(realbinary)]
+        
+    zerocount = 0
+    onecount = 0
+    binarycount += 1
+
+binarycount = 0
+
+while binarycount < 12:
+    for u in co2binarylist:
+        if u[binarycount] == "0":
+            zerocount += 1
+        elif u[binarycount] == "1":
+            onecount += 1
+
+    if zerocount > onecount:
+        co2binary += "1"
+    elif onecount > zerocount or onecount == zerocount:
+        co2binary += "0"
+
+    if len(co2binarylist) > 1:
+        co2binarylist = [x for x in co2binarylist if x.startswith(co2binary)]
+
+    zerocount = 0
+    onecount = 0
+    binarycount += 1
+
+lifesupportrating = int(Binary[0], 2) * int(co2binarylist[0], 2)
+print(lifesupportrating)
