c1cb97446112123232e84009d5b15a7c233ad5bb
Sebbern <Sebastianeckbo@gmail.com>
2022-10-15 21:15:40 +0200

    add day 3, java

---
+++ 2015/day03/day03_2.java
@@ -0,0 +1,66 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+
+public class day03_2 {
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+        File inputtxt = new File("2015\\day03\\input.txt");
+        BufferedReader input = new BufferedReader(new FileReader(inputtxt));
+        String string;
+        ArrayList<String> coordinates = new ArrayList<String>();
+        int x = 0;
+        int y = 0;
+        int roboX = 0;
+        int roboY = 0;
+        int turn = 0;
+
+        while ((string = input.readLine()) != null){
+            char[] symbol = new char[string.length()];
+            for (int i=0; i<string.length(); i++){
+                symbol[i] = string.charAt(i);
+                String symbolString = Character.toString(symbol[i]);
+
+                if (turn == 0){
+                    if (symbolString.equals("<")){
+                        x -= 1;
+                    }
+                    else if (symbolString.equals(">")){
+                        x += 1;
+                    }
+                    else if (symbolString.equals("v")){
+                        y -= 1;
+                    }
+                    else if (symbolString.equals("^")){
+                        y += 1;
+                    }
+                    coordinates.add(x+","+y);
+                    turn += 1;
+                }
+
+                else if (turn == 1){
+                    if (symbolString.equals("<")){
+                        roboX -= 1;
+                    }
+                    else if (symbolString.equals(">")){
+                        roboX += 1;
+                    }
+                    else if (symbolString.equals("v")){
+                        roboY -= 1;
+                    }
+                    else if (symbolString.equals("^")){
+                        roboY += 1;
+                    }
+                    turn -= 1;
+                    coordinates.add(roboX+","+roboY);
+                }
+            }
+            HashSet<String> uniqueCoordinates = new HashSet<String>(coordinates);
+            System.out.println(uniqueCoordinates.size());
+        }
+        input.close();
+    }
+}
