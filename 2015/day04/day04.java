1cd4121471e20c5e4f8509710ea0635ee08d4c1b
Sebbern <Sebastianeckbo@gmail.com>
2022-10-17 00:26:35 +0200

    change hexa into StringBuilder()

--- 2015/day04/day04.java
+++ 2015/day04/day04.java
@@ -1,31 +1,32 @@
 import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 public class day04 {
     public static void main(String[] args) throws UnsupportedEncodingException, NoSuchAlgorithmException {
         String input = "yzbqklnj";
-        String hexa;
+        StringBuilder hexa;
         String editedInput;
         int count = 0;
         
         while (true){
             editedInput = (input+count);
-            byte[] bytesInput = editedInput.getBytes("UTF-8");
+            byte[] bytesInput = editedInput.getBytes(StandardCharsets.UTF_8);
             MessageDigest md = MessageDigest.getInstance("MD5");
             byte[] mdArray = md.digest(bytesInput);
-            hexa = "";
+            hexa = new StringBuilder();
 
             for (byte i : mdArray){
-                hexa += String.format("%02x", i);
-                if (!hexa.startsWith("0")){
+                hexa.append(String.format("%02x", i));
+                if (!hexa.toString().startsWith("0")){
                     break;
                 }
             }
-            
-            // Task 1: (hexa.startsWith("00000")
-            // Task 2: (hexa.startsWith("000000")
-            if (hexa.startsWith("000000")){
+
+            // Task 1: (hexa.toString().startsWith("00000")
+            // Task 2: (hexa.toString().startsWith("000000")
+            if (hexa.toString().startsWith("00000")){
                 System.out.println(editedInput);
                 System.out.println(hexa);
                 System.out.println(count);
