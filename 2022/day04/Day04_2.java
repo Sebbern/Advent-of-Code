9cb5ba2634a9cb9ff5029bc04dd89c5b58b5fbe7
Sebbern <Sebastianeckbo@gmail.com>
2022-12-04 14:38:26 +0100

    add day4 2022, java

---
+++ 2022/day04/Day04_2.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Day04_2 {
+    private static int check(String x, String y){
+        int elfOneLow,elfOneHigh,elfTwoLow,elfTwoHigh;
+        String[] xSplit, ySplit;
+        xSplit = x.split("-");
+        ySplit = y.split("-");
+        elfOneLow = Integer.parseInt(xSplit[0]);
+        elfOneHigh = Integer.parseInt(xSplit[1]);
+        elfTwoLow = Integer.parseInt(ySplit[0]);
+        elfTwoHigh = Integer.parseInt(ySplit[1]);
+
+        if (((elfOneLow <= elfTwoLow) && (elfTwoLow <= elfOneHigh)) || ((elfTwoLow <= elfOneLow) && (elfOneLow <= elfTwoHigh))){
+            return 1;
+        }
+        else {
+            return 0;
+        }
+    }
+
+    public static void main(String[] args) throws IOException{
+        File inputTxt = new File("2022\\day04\\input.txt");
+        BufferedReader input = new BufferedReader(new FileReader(inputTxt));
+        String string, x, y;
+        ArrayList<String> assignmentList = new ArrayList<>();
+        String[] split;
+
+        while ((string = input.readLine()) != null){
+            assignmentList.add(string);
+        }
+        input.close();
+        int pairs = 0;
+
+        for (String i: assignmentList){
+            split = i.split(",");
+            x = split[0];
+            y = split[1];
+
+            pairs += check(x,y);
+        }
+        System.out.println(pairs);
+    }
+}
