a5d5587da431fe1e92eb85effda5af07ff17db20
Sebbern <Sebastianeckbo@gmail.com>
2024-12-05 22:27:51 +0100

    Fix code readability

--- 2024/day02/day02.py
+++ 2024/day02/day02.py
@@ -3,25 +3,25 @@ input = open("2024\\day02\\input.txt").read().splitlines()
 safe = 0
 
 for i in input:
-    input_list = i.split(" ")
-    if int(input_list[0]) > int(input_list[-1]):
+    input_list = list(map(int,i.split(" ")))
+    if input_list[0] > input_list[-1]:
         for u in range(len(input_list)):
             if u == len(input_list)-1:
                 safe += 1
                 break
 
-            if (int(input_list[u]) > int(input_list[u+1])) and (1 <= (int(input_list[u]) - int(input_list[u+1])) <= 3):
+            if input_list[u] > input_list[u+1] and 1 <= (input_list[u] - input_list[u+1]) <= 3:
                 continue
             else:
                 break
 
-    elif int(input_list[0]) < int(input_list[-1]):
+    elif input_list[0] < input_list[-1]:
         for u in range(len(input_list)):
             if u == len(input_list)-1:
                 safe += 1
                 break
 
-            if (int(input_list[u]) < int(input_list[u+1])) and (1 <= (int(input_list[u+1]) - int(input_list[u])) <= 3):
+            if input_list[u] < input_list[u+1] and 1 <= (input_list[u+1] - input_list[u]) <= 3:
                 continue
             else:
                 break
